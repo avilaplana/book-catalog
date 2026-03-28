29cdf1ab3cf69bcbc9ce0581eb1617f31828e7d1
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmail.com>
2026-03-28 18:27:18 +0000

    fix(mobile): fix scaffolding config issues

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'jest-expo',
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@tanstack/.*|zustand)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@tanstack/.*|zustand|axios)',
   ],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
